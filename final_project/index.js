a6af336a508df3a7fa4d38865d18413ca2e66668
Ricardo Vano <ricardo.vano@gmail.com>
2024-05-21 16:08:50 -0300

    first commit

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,10 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (!req.session.user) {
+        return res.status(401).json({ message: 'Unauthorized: Please log in' });
+    }
+    next();
 });
  
 const PORT =5000;
