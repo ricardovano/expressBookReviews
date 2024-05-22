f82d3b34f6c116c12591e7cfc81549829ed28fd6
Ricardo Vano <ricardo.vano@gmail.com>
2024-05-22 10:03:07 -0300

    adding search by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,41 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books[req.params.isbn]))
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const name = req.params.author
+  filteredBooks = []
+
+  let i = 1
+  while (books[i] != undefined){
+    if (books[i].author === name){
+      filteredBooks.push(books[i])
+    }
+    i++
+  }
+  return res.json(filteredBooks)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const name = req.params.title
+  filteredBooks = []
+  let i = 1
+  while (books[i] != undefined){
+    if (books[i].title === name){
+      filteredBooks.push(books[i])
+    }
+    i++
+  }
+  return res.json(filteredBooks)
 });
 
 //  Get book review
