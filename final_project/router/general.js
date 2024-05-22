431bd57cabb152b63f17781494552290bf527001
Ricardo Vano <ricardo.vano@gmail.com>
2024-05-22 10:08:24 -0300

    adding get review from a book

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,8 +51,11 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn
+  let reviews = books[req.params.isbn].reviews
+  return res.json(reviews)
+  
 });
 
 module.exports.general = public_users;
