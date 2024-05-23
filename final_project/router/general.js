4b334e6a2f456dbc80e636ecd47132ccd4a1f7ef
Ricardo Vano <ricardo.vano@gmail.com>
2024-05-23 09:22:31 -0300

    adding async book by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,9 +19,8 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
-public_users.get('/asynbooks/',function (req, res) {
+public_users.get('/async/',async function (req, res) {
 
-  async function getBooks() {
     try {
       const response = await axios.get("http://localhost:5001/");
       return res.json(response.data);
@@ -29,15 +28,23 @@ public_users.get('/asynbooks/',function (req, res) {
       console.error('Error fetching books:', error);
       res.status(500).json({ message: 'Failed to retrieve books' });
     }
-  }
-
-  getBooks();
 
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   return res.send(JSON.stringify(books))
+});
+
+public_users.get('/async/isbn/:isbn', async function (req, res) {
+  const isbn = req.params.isbn
+  try {
+    const response = await axios.get(`http://localhost:5001/isbn/${isbn}`);
+    return res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    res.status(500).json({ message: 'Failed to retrieve books' });
+  }
 });
 
 // Get book details based on ISBN
