d2dc67424abf982c54694a51f7710523f4a3de81
Ricardo Vano <ricardo.vano@gmail.com>
2024-05-23 09:43:45 -0300

    adding async book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
-const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
+const axios = require('axios')
+const express = require('express')
+let books = require("./booksdb.js")
+let isValid = require("./auth_users.js").isValid
+let users = require("./auth_users.js").users
+const public_users = express.Router()
 
 public_users.post("/register", (req,res) => {
   const username = req.body.username
@@ -11,22 +11,22 @@ public_users.post("/register", (req,res) => {
   if (username && password) {
     if (!isValid(username)) {
       users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+      return res.status(200).json({message: "User successfully registred. Now you can login"})
     } else {
-      return res.status(404).json({message: "User already exists!"});
+      return res.status(404).json({message: "User already exists!"})
     }
   }
-  return res.status(404).json({message: "Unable to register user."});
+  return res.status(404).json({message: "Unable to register user."})
 });
 
+//Get all books using async
 public_users.get('/async/',async function (req, res) {
 
     try {
       const response = await axios.get("http://localhost:5001/");
       return res.json(response.data);
     } catch (error) {
-      console.error('Error fetching books:', error);
-      res.status(500).json({ message: 'Failed to retrieve books' });
+      res.status(500).json({ message: 'Failed to retrieve books' })
     }
 
 });
@@ -36,21 +36,44 @@ public_users.get('/',function (req, res) {
   return res.send(JSON.stringify(books))
 });
 
+// Get book details based on ISBN using async
 public_users.get('/async/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn
   try {
-    const response = await axios.get(`http://localhost:5001/isbn/${isbn}`);
-    return res.json(response.data);
+    const response = await axios.get(`http://localhost:5001/isbn/${isbn}`)
+    return res.json(response.data)
   } catch (error) {
-    console.error('Error fetching books:', error);
-    res.status(500).json({ message: 'Failed to retrieve books' });
+    res.status(500).json({ message: 'Failed to retrieve books' })
   }
+});
+
+//Get book details based on ISBN using promise callbacks
+public_users.get('/promise/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  axios.get(`http://localhost:5001/isbn/${isbn}`)
+    .then(response => {
+      return res.json(response.data)
+    })
+    .catch(error => {
+      return res.status(500).json({ message: 'Failed to retrieve books' })
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   return res.send(JSON.stringify(books[req.params.isbn]))
  });
+
+ public_users.get('/async/author/:author', async function (req, res) {
+  const name = req.params.author
+  try {
+    const response = await axios.get(`http://localhost:5001/author/${name}`)
+    return res.json(response.data)
+  }  
+  catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve books' })
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -66,6 +89,18 @@ public_users.get('/author/:author',function (req, res) {
   }
   return res.json(filteredBooks)
 });
+
+//Get book details based on title using async
+public_users.get('/async/title/:title', async function(req, res){
+  const title = req.params.title
+  try {
+    const response = await axios.get(`http://localhost:5001/title/${title}`)
+    return res.json(response.data)
+  }
+  catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve books' })
+  }
+})
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
